af3bc52286cb59518d523219ad445a058b8c2cea
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2024-05-18 20:40:50 +0200

    Implemented player extension for language/color easy access

---
+++ base-api/src/main/kotlin/world/neptuns/core/base/api/extension/NeptunPlayerExtension.kt
@@ -0,0 +1,31 @@
+package world.neptuns.core.base.api.extension
+
+import net.kyori.adventure.text.format.TextColor
+import world.neptuns.core.base.api.NeptunCoreProvider
+import world.neptuns.core.base.api.language.Language
+import world.neptuns.core.base.api.language.properties.LanguageProperties
+import world.neptuns.core.base.api.player.NeptunOfflinePlayer
+
+suspend fun NeptunOfflinePlayer.getLanguage(): Language? {
+    val languageProperties = getLanguageProperties() ?: return null
+    return NeptunCoreProvider.api.languageController.getLanguage(languageProperties.langKey)
+}
+
+suspend fun NeptunOfflinePlayer.getPrimaryColor(): TextColor? {
+    val languageProperties = getLanguageProperties() ?: return null
+    return languageProperties.primaryColor
+}
+
+suspend fun NeptunOfflinePlayer.getSecondaryColor(): TextColor? {
+    val languageProperties = getLanguageProperties() ?: return null
+    return languageProperties.secondaryColor
+}
+
+suspend fun NeptunOfflinePlayer.getSeparatorColor(): TextColor? {
+    val languageProperties = getLanguageProperties() ?: return null
+    return languageProperties.separatorColor
+}
+
+suspend fun NeptunOfflinePlayer.getLanguageProperties(): LanguageProperties? {
+    return NeptunCoreProvider.api.languagePropertiesController.getPropertiesAsync(this.uuid).await()
+}
